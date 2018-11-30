f352cd6fc2c76fef875bd76333df73056934abd9
muna <muna.1998.01.08@gmail.com>
2018-11-30 20:31:14 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+
+  has_secure_password
+
+  validates :name,{presence: true}
+  validates :email,{presence:true,uniqueness:true}
+
+  def posts
+    return Post.where(user_id: self.id)
+  end
+end
